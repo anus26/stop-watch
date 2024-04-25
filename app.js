a3c2ee0a0f6ed1f644d525831a36e28b26512d56
anas-raza~ <anusraza268@gmail.com>
2024-04-24 23:27:44 -0700

    index file created

---
+++ app.js
@@ -0,0 +1,22 @@
+// const second =document.querySelector('#sec');
+// const minutes =document.querySelector('#min')
+// function startWatch(){
+//     let sec = 0
+//     setInterval(function(){
+//         sec += 1
+//         second.innerHTML = sec
+//     },100)
+// }
+// function stopInterval(){
+//     let min = 0
+//     setInterval(function(){
+//         min += 1
+//         minutes.innerHTML = min
+
+//     },1)
+// }
+
+// // function stopInterval(){
+// //     clearInterval(seconds)
+// //     clearTimeout(second)
+// // }
